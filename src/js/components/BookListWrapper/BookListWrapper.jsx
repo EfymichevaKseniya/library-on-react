1b27f8c8577d33c57a8f7b07da8386b93e2ae5e2
Q-digital <no-reply@q-digital.org>
2022-04-29 11:56:36 +0400

    Replace to src/js component BookListWrapper

---
+++ src/js/components/BookListWrapper/BookListWrapper.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable react/static-property-placement */
+import React from 'react';
+import BooksContext from '../../../utils/BooksContext';
+import BookCard from '../BookCard/AddBookCard';
+import { Modal } from '../Modal/Modal';
+import styles from './bookList.module.scss';
+
+class BookListWrapper extends React.Component {
+  static contextType = BooksContext;
+
+  constructor(props) {
+    super(props);
+    this.showModal = this.showModal.bind(this);
+    this.addBook = this.addBook.bind(this);
+    this.state = { idForShelf: '', modalOpen: false };
+  }
+
+  showModal() {
+    this.setState((prevState) => ({
+      modalOpen: !prevState.modalOpen,
+    }));
+  }
+
+  addBook(e) {
+    this.setState({ idForShelf: e });
+  }
+
+  render() {
+    const { books } = this.context;
+    const { modalOpen, idForShelf } = this.state;
+
+    return (
+      <>
+        <ul className={styles.book__list}>
+          {books.length !== 0 &&
+            books.map((book, index) => {
+              return (
+                <BookCard
+                  id={book.id}
+                  title={book.volumeInfo.title}
+                  key={book.id}
+                  index={index + 1}
+                  modalOpen={modalOpen}
+                  showModal={this.showModal}
+                  addBook={this.addBook}
+                />
+              );
+            })}
+        </ul>
+        <Modal
+          modalOpen={modalOpen}
+          id={idForShelf}
+          title='Add'
+          content='add'
+          showModal={this.showModal}
+        />
+      </>
+    );
+  }
+}
+
+export default BookListWrapper;
---
+++ src/js/components/BookListWrapper/BookListWrapperForMyBooks.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { BookCardInList } from '../BookCard/BookCardInList';
+import { ModalDeleteBook } from '../Modal/ModalDeleteBook';
+import styles from './bookList.module.scss';
+
+export class BookListWrapperForMyBooks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.showModal = this.showModal.bind(this);
+    this.deleteBook = this.deleteBook.bind(this);
+    this.state = { idForShelf: '', modalOpen: false };
+  }
+
+  showModal() {
+    this.setState((prevState) => ({
+      modalOpen: !prevState.modalOpen,
+    }));
+  }
+
+  deleteBook(e) {
+    this.setState({ idForShelf: e });
+  }
+
+  render() {
+    // const books  = this.props.booksShelf;
+    const { modalOpen, idForShelf } = this.state;
+    const { books } = this.props;
+    // console.log(this.books);
+
+    return (
+      <>
+        <ul className={styles.book__list}>
+          {books &&
+            books.map((book, index) => {
+              return (
+                <BookCardInList
+                  id={book.id}
+                  title={book.title}
+                  key={book.id}
+                  index={index + 1}
+                  modalOpen={modalOpen}
+                  favorite={book.favorite}
+                  showModal={this.showModal}
+                  deleteBook={this.deleteBook}
+                />
+              );
+            })}
+        </ul>
+        <ModalDeleteBook
+          modalOpen={modalOpen}
+          id={idForShelf}
+          title='Delete'
+          content='delete'
+          showModal={this.showModal}
+        />
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  ...state,
+});
+
+export default connect(mapStateToProps)(BookListWrapperForMyBooks);
